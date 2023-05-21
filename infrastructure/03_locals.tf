3947c19c8a19f81d91e76caba0cead4fddd1415f
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2023-05-21 15:53:31 +0200

    Syntaxe

--- infrastructure/03_locals.tf
+++ infrastructure/03_locals.tf
@@ -2,17 +2,18 @@
 ###############################################################
 locals {
   hosts = {
-    "grafana"  = "grafana.arig.org"
-    "postgres" = "pg.arig.org"
+    grafana  = "grafana.arig.org"
+    postgres = "pg.arig.org"
+    influxdb = "influxdb.arig.org"
   }
 
   users = {
-    "admin_username"    = "admin"
-    "admin_password"    = "admin"
-    "arig_username"     = "arig"
-    "arig_password"     = "arig"
-    "postgres_username" = "postgres"
-    "postgres_password" = "postgres"
+    admin_username    = "admin"
+    admin_password    = "admin"
+    arig_username     = "arig"
+    arig_password     = "arig4ever"
+    postgres_username = "postgres"
+    postgres_password = "postgres"
   }
 
   bddName = "robots"
